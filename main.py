67696c78a06b86d5efd1dea60957002d2e976c60
Rocks-s-s-s <oleg.martynov2008@gmail.com>
2022-07-07 15:40:09 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ContentType, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils import executor
+from aiogram.utils.emoji import emojize
+from config import TOKEN
+
+
