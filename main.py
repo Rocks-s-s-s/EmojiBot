19633497fb4dff4b2c17b128863fba6f0c0d4eb2
Rocks-s-s-s <oleg.martynov2008@gmail.com>
2022-08-24 16:59:21 +0300

    Add git ignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import dp as dp
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ContentType, KeyboardButton, ReplyKeyboardMarkup
@@ -5,4 +6,26 @@ from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
 from config import TOKEN
 
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+y = 0
+emojifile = open('emoji','r')
+phrasesfile = open('phrases','r')
 
+emoji = [emojifile]
+phrases = [phrasesfile]
+
+@dp.message_handler()
+async def get_message(message: types.Message):
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
