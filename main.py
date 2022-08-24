113ed7ee41c7fa85316622464ee7f4709b037d25
a.boev.it@gmail.com <#gi32@+1>
2022-08-24 17:09:09 +0300

    ДЗ 24.08.2022

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import dp as dp
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ContentType, KeyboardButton, ReplyKeyboardMarkup
@@ -6,25 +5,33 @@ from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
 from config import TOKEN
 
+'''
+    EmojiBot
+    Может работать по двум алгоритмам:
+    1. Вставляет смайлы после известной фразы.
+    2. Вставляет смайлы вместо известной фразы.
+'''
+
+#TODO: 0. Обдумать как будет происходить процесс вставки смайла в текст.
+#TODO: 1. Считать файлы с данными, так, чтобы удобно было потом работать.
+#TODO: 2. Реализовать первый алгоритм, который вставляет после известной фразы смайл.
+#TODO: 3. Добавить на каждую фразу по нескольку смайлов (пока у нас по одному на каждую).
+
+
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+# Странная переменная, для чего этооооооооооо?
 y = 0
-emojifile = open('emoji','r')
-phrasesfile = open('phrases','r')
+emojifile = open('emoji', 'r')
+phrasesfile = open('phrases', 'r')
 
 emoji = [emojifile]
 phrases = [phrasesfile]
 
 @dp.message_handler()
 async def get_message(message: types.Message):
-
-
-
-
-
-
-
-
+    pass
 
 
 if __name__ == '__main__':
