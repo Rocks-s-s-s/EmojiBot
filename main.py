d72b28fee4be23fa865b8c2dbeaefa2eb1ef2ba5
a.boev.it@gmail.com <#gi32@+1>
2022-09-19 20:45:14 +0300

    Добавлено задание (TODO 5) от 19.09.2022

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ from config import TOKEN
 # TODO: 1. +Считать файлы с данными, так, чтобы удобно было потом работать.
 # TODO: 2. +Реализовать первый алгоритм, который вставляет после известной фразы смайл.
 # TODO: 3. Добавить на каждую фразу по нескольку смайлов (пока у нас по одному на каждую).
-# TODO: 4. Реализовать второй алгоритм, который заменяет фразу на смайл
+# TODO: 4. +Реализовать второй алгоритм, который заменяет фразу на смайл
+# TODO: 5. Добавить словарь пользователей. После каждого текстового сообщения добавлять пользователя и его текст в словарь.
+#          Если пользователь в словаре есть, то заменять его текст.
+#          После нажатия кнопки type 1 / type 2, анализировать id пользователя, брать его текст и выводить преобразованный.
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
 typeKeyboard = types.InlineKeyboardMarkup()
-type1Button = types.InlineKeyboardButton(text='type 1', callback_data='type1Button_click')
-type2Button = types.InlineKeyboardButton(text='type 2', callback_data='type2Button_click')
+type1Button = types.InlineKeyboardButton(text=emojize('hi, world! ➝ hi:wave:, world!'),
+                                         callback_data='type1Button_click')
+type2Button = types.InlineKeyboardButton(text=emojize('hi, world! ➝ :wave:, world!'),
+                                         callback_data='type2Button_click')
 typeKeyboard.add(type1Button, type2Button)
 
 emojifile = open('emoji.txt', 'r')
@@ -58,13 +64,17 @@ def emojizeText2(text):
 @dp.message_handler()
 async def get_message(message: types.Message):
     await message.answer('Выберите тип', reply_markup=typeKeyboard)
+
+
 @dp.callback_query_handler(text='type1Button_click')
 async def type1Button_click(call: types.CallbackQuery):
     await call.message.answer(emojizeText1(call.message.text))
 
+
 @dp.callback_query_handler(text='type2Button_click')
 async def type2Button_click(call: types.CallbackQuery):
     await call.message.answer(emojizeText2(call.message.text))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
