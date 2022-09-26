c1120118383a6c11e2284ef0353db2ddda0081b0
a.boev.it@gmail.com <#gi32@+1>
2022-09-26 20:29:45 +0300

    Добавлена реакция на нажатие кнопок. Бот версии 1.0 завершён.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ContentType, KeyboardButton, ReplyKeyboardMarkup
@@ -31,6 +30,7 @@ phrasesfile = open('phrases.txt', 'r')
 
 emoji = []
 phrases = []
+users = {}
 
 for line in phrasesfile:
     phrases.append(line.strip().split('#'))
@@ -42,7 +42,7 @@ def emojizeText1(text):
     line = 0
     for phrase in phrases:
         for p in phrase:
-            pos = text.find(p)
+            pos = text.lower().find(p)
             if pos > -1:
                 pos = pos + len(p)
                 text = text[:pos] + random.choice(emoji[line]) + text[pos + 1:]
@@ -54,7 +54,7 @@ def emojizeText2(text):
     line = 0
     for phrase in phrases:
         for p in phrase:
-            pos = text.find(p)
+            pos = text.lower().find(p)
             if pos > -1:
                 text = text[:pos] + random.choice(emoji[line]) + text[pos + len(p):]
         line = line + 1
@@ -63,17 +63,29 @@ def emojizeText2(text):
 
 @dp.message_handler()
 async def get_message(message: types.Message):
+    users[message.from_user.id] = message.text
+    print(users)
     await message.answer('Выберите тип', reply_markup=typeKeyboard)
 
 
 @dp.callback_query_handler(text='type1Button_click')
 async def type1Button_click(call: types.CallbackQuery):
-    await call.message.answer(emojizeText1(call.message.text))
+    if call.from_user.id in users:
+        text = users[call.from_user.id]
+        eText = emojizeText1(text)
+        await call.message.answer(eText)
+    else:
+        await call.message.answer('Введите текст')
 
 
 @dp.callback_query_handler(text='type2Button_click')
 async def type2Button_click(call: types.CallbackQuery):
-    await call.message.answer(emojizeText2(call.message.text))
+    if call.from_user.id in users:
+        text = users[call.from_user.id]
+        eText = emojizeText2(text)
+        await call.message.answer(eText)
+    else:
+        await call.message.answer('Введите текст')
 
 
 if __name__ == '__main__':
